4165b3b25f8d2f1232000f9e7d686c8199db779a
Durell Hart <rellsadmin@Durells-MacBook-Pro.local>
2024-05-07 21:21:45 -0400

    pushing linux basics

---
+++ linux_basics.sh
@@ -0,0 +1,14 @@
+#!/bin/zsh
+#git remote show origin
+#git remote set-url origin <url>
+#git remote set-url --push origin <url>
+#------------------------------------------------------------------------------------------------------------------------------#
+echo Hi #Print to screen
+ls #List files and folders
+cd my_dir1 #Change directory 
+pwd #Present Working Directory
+mkdir new_directory #Make Directory
+cd new_directory; mkdir www; pwd #Multiple commands
+mkdir -p /tmp/asia/india/bandalore #Make Directory Hierarchy
+rm -r /tmp/my_dir1 #Remove Directory
+cp -r my_dir1 /tmp/my_dir1 #Copy a directory and all of it's contents from one location to another
