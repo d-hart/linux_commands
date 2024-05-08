b4d7b11849296a043c1801ecd61d4b9adfcf79b3
d-smart <durellhart@live.com>
2024-05-07 21:40:42 -0400

    commit key test

--- linux_basics.sh
+++ linux_basics.sh
@@ -12,4 +12,4 @@ cd new_directory; mkdir www; pwd #Multiple commands
 mkdir -p /tmp/asia/india/bandalore #Make Directory Hierarchy
 rm -r /tmp/my_dir1 #Remove Directory
 cp -r my_dir1 /tmp/my_dir1 #Copy a directory and all of it's contents from one location to another
-test
+
